e1974b804c7554d06296e4ae1e8290849c72dab4
Мөнхсарнай Буяндэлгэр <24LP2620@G401-CC0213.local>
2024-10-08 12:36:32 +0800

    mash butelgui udur

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,32 +1,24 @@
-import { BACKEND_ENDPOINT } from "@/constants/constant";
-
-export const Card = ({
-  product,
-  selectedProduct,
-  setSelectedProduct,
-  setProducts,
-}) => {
-  const { productName, price } = product;
-
+const Card = ({ productName, price }) => {
   return (
-    <div class="card bg-base-100 w-96 shadow-xl">
+    <div className="shadow-xl card bg-base-100 w-96">
       <figure>
         <img
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt="Shoes"
         />
       </figure>
-      <div class="card-body">
-        <h2 class="card-title">{productName}</h2>
+      <div className="card-body">
+        <h2 className="card-title">{productName}</h2>
         <div className="flex justify-between">
           <p>{price}$</p>
         </div>
       </div>
       <div className="justify-end p-4 pt-0 card-actions">
-        <button onClick={() => handleDelete(product?.id)} className="btn">
-          Delete
-        </button>
+        <button className="btn">Delete</button>
+        {/* <button onClick={()=>}></button> */}
       </div>
     </div>
   );
 };
+
+export default Card;
--- front-end/src/components/index.jsx
+++ front-end/src/components/index.jsx
@@ -1,2 +1,1 @@
-export * from "./CreateModal";
 export * from "./Card";
